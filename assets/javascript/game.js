fc60c728aed72d0de0a4575825ed09a7ade6a515
Lindsey Garland <rebeccgaupo@gmail.com>
2018-05-08 16:48:59 -0700

    added image and some poorly written code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,16 @@
 var wins = 0;
 var losses = 0;
 var options = ["discworld", "vetinari", "octarine", "headology"];
+
+var blank = []
+
+var thisRound = options[Math.floor(Math.random()*options.length)];
+            
+
+/* var correct = thisRound.split (“”);
+                
+for i=0; i < correct.length, i++ {
+                Blank.push (“_”);
+}
+
+document.getElementById(“blank”).innerHTML = blank */
